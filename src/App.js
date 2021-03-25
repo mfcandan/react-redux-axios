f5fcd3adf4da5e3302bb499349125234335bf052
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-03-25 14:50:31 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import Ninjas from "./Ninjas";
 import {useState} from 'react';
 function App() {
   const [ninjas, setNinjas] = useState([
-    {name:'fatih',age:25,belt:'blue'},
-    {name:'orçun',age:30,belt:'black'}
+    {name:'fatih',age:22,belt:'blue',id:1},
+    {name:'orçun',age:18,belt:'black',id:2}
   ])
   return (
     <div className="App">
       <h2>React - Redux - Axios App</h2>
       <Ninjas ninjas={ninjas}/>
-
     </div>
   );
 }
