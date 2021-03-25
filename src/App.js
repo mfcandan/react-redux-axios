47d72b347b25dffc0e7d95b199698854d91a1587
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-03-25 14:40:48 +0300

    usestate ninjalist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Ninjas from "./Ninjas";
+import {useState} from 'react';
 function App() {
+  const [ninjas, setNinjas] = useState([
+    {name:'fatih',age:25,belt:'blue'},
+    {name:'orçun',age:30,belt:'black'}
+  ])
   return (
     <div className="App">
       <h2>React - Redux - Axios App</h2>
+      <Ninjas ninjas={ninjas}/>
+
     </div>
   );
 }
