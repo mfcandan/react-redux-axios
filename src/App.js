064846e14313233f41b691c955b7c5bf801e6d2e
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-03-25 15:03:21 +0300

    app js upt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import Ninjas from "./Ninjas";
-import {useState} from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas'
 import AddNinja from './AddNinja'
-function App() {
-  const [ninjas, setNinjas] = useState([
-    {name:'fatih',age:22,belt:'blue',id:1},
-    {name:'orçun',age:18,belt:'black',id:2}
-  ])
-  return (
-    <div className="App">
-      <h2>React - Redux - Axios App</h2>
-      <Ninjas ninjas={ninjas}/>
-      <AddNinja />
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Ninjas ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja} />
+      </div>
+    );
+  }
 }
 
 export default App;
